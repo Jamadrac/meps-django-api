5a143065d09273493f62c449b97cc0a3953c642d
jamadrac <rabbikindalo001@@gmale.com>
2023-09-03 12:08:39 +0200

    cleaned

--- base/api/views.py
+++ base/api/views.py
@@ -23,48 +23,34 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
         token['username'] = user.username
-
         return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-
-
-
-@api_view(['PUT'])  # You can also use 'PATCH' for partial updates
+@api_view(['PUT'])  
 @permission_classes([IsAuthenticated])
 def get_profile(request):
     user = request.user
     profile = user.profile
-
-    # Serialize and update the profile data
     serializer = ProfileSerializer(profile, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx GPS xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 @api_view(['POST'])
 def receive_location_data(request):
     try:
         latitude = request.data['latitude']
         longitude = request.data['longitude']
         build_number = request.data['buildNumber']
-
-        # Save the received data to the database
         LocationData.objects.create(
             latitude=latitude,
             longitude=longitude,
             build_number=build_number,
         )
-
         return Response({'message': 'Location data received and stored successfully.'}, status=status.HTTP_201_CREATED)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
